d6d21180b4b6b8b629544fb4765f53515f8d2b91
ql-eilde <quentin.leilde@gmail.com>
2015-05-14 20:18:58 +0200

    add validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514171803) do
+ActiveRecord::Schema.define(version: 20150514175352) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20150514171803) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
