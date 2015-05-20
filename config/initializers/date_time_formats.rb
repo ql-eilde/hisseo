bcb1ec63bf647338aeb98cf7c25434dd1892bb78
ql-eilde <quentin.leilde@gmail.com>
2015-05-20 17:11:04 +0200

    remove date time formats

--- config/initializers/date_time_formats.rb
+++
@@ -1,9 +0,0 @@
-Date::DATE_FORMATS[:default] = '%d/%m/%Y'
-
-Date::DATE_FORMATS.merge!({:db => '%d/%m/%Y', :fr_format => '%d/%m/%Y'})
-
-# if you want to change the format of Time display then add the line below
-Time::DATE_FORMATS[:default]= '%H:%M'
-
-# if you want to change the DB date format.
-Time::DATE_FORMATS.merge!({:db => '%Y-%m-%d %H:%M:%S %z'})
